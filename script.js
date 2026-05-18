67c7a97075da2e5c6b49bf0e7f74f664b45a5587
imonclus <46054915+imonclus@users.noreply.github.com>
2026-05-18 14:52:51 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+const backToTopButton = document.querySelector('.back-to-top');
+
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 300) {
+        backToTopButton.classList.add('show');
+    } else {
+        backToTopButton.classList.remove('show');
+    }
+});
